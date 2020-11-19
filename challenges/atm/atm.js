ff36ccc5ec2a008ad449e7ac8a6ef611b0d81a45
Carlos Felício <felicio.contato@live.com>
2020-11-18 23:37:53 -0300

    Finished challenge calculator

--- challenges/atm/atm.js
+++ challenges/atm/atm.js
@@ -27,3 +27,109 @@
 
 // Entrada: NULL
 // Resultado: Erro de valor nulo
+
+const n0 = document.getElementById('n0');
+const n1 = document.getElementById('n1');
+const n2 = document.getElementById('n2');
+const n3 = document.getElementById('n3');
+const n4 = document.getElementById('n4');
+const n5 = document.getElementById('n5');
+const n6 = document.getElementById('n6');
+const n7 = document.getElementById('n7');
+const n8 = document.getElementById('n8');
+const n9 = document.getElementById('n9');
+const btnClear = document.getElementById('clear');
+const btnConfirm = document.getElementById('confirm');
+const billsDisplayed = document.getElementById('bills-displayer');
+let amountToWithdraw = document.getElementById('displayer');
+const balance = document.getElementsByClassName('value');
+
+n0.addEventListener('click', () => insertIntoDisplay(n0));
+n1.addEventListener('click', () => insertIntoDisplay(n1));
+n2.addEventListener('click', () => insertIntoDisplay(n2));
+n3.addEventListener('click', () => insertIntoDisplay(n3));
+n4.addEventListener('click', () => insertIntoDisplay(n4));
+n5.addEventListener('click', () => insertIntoDisplay(n5));
+n6.addEventListener('click', () => insertIntoDisplay(n6));
+n7.addEventListener('click', () => insertIntoDisplay(n7));
+n8.addEventListener('click', () => insertIntoDisplay(n8));
+n9.addEventListener('click', () => insertIntoDisplay(n9));
+
+btnClear.addEventListener('click', () => {
+    amountToWithdraw.value = '';
+    billsDisplayed.innerHTML = '';
+});
+
+btnConfirm.addEventListener('click', () => {
+    const value = Number.parseInt(amountToWithdraw.value);
+    let stringValue = validateString(amountToWithdraw.value);
+
+    if (value < 0) {
+        amount = 'Invalid value';
+        clearDisplay();
+    } else if (amountToWithdraw.value === '') {
+        amountToWithdraw.value = 'Null value';
+        clearDisplay();
+    } else if (stringValue == false) {
+        amountToWithdraw.value = 'Banknotes indisponible';
+        clearDisplay();
+    } else {
+        const bankNotes = {
+            oneHundredBanknotes: {
+                valueNotes: '100.00',
+                qtdBanknotes: ''
+            },
+            fiftyBanknotes: {
+                valueNotes: '50.00',
+                qtdBanknotes: ''
+            },
+            twentyBanknotes: {
+                valueNotes: '20.00',
+                qtdBanknotes: ''
+            },
+            tenBanknotes: {
+                valueNotes: '10.00',
+                qtdBanknotes: ''
+            }
+        }
+
+        const totalBankNotes = [bankNotes.oneHundredBanknotes, bankNotes.fiftyBanknotes, bankNotes.twentyBanknotes, bankNotes.tenBanknotes];
+
+        for(let i = 0; i < 4; i++) {
+            totalBankNotes[i].qtdBanknotes = qtdNotes(amountToWithdraw.value, totalBankNotes[i].valueNotes);
+            if(totalBankNotes[i].qtdBanknotes > 0) {
+                billsDisplayed.innerHTML = billsDisplayed.innerHTML + `[${totalBankNotes[i].qtdBanknotes}x $ ${totalBankNotes[i].valueNotes}] `;
+            }
+        }
+
+        setTimeout(() => {
+            amountToWithdraw.value = ''
+            billsDisplayed.innerHTML = '';
+        }, 5000);
+    }
+});
+
+function insertIntoDisplay(element) {
+    amountToWithdraw.value = amountToWithdraw.value + element.innerHTML;
+}
+
+function qtdNotes(value, noteValue) {
+    const qtd = Math.trunc(value / noteValue);
+    amountToWithdraw.value = value - (qtd * noteValue);
+    return qtd;
+}
+
+function validateString(string) {
+    const isValid = string.substr((string.length - 1), 1);
+    if( isValid === '0') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function clearDisplay() {
+    setTimeout(() => {
+        amountToWithdraw.value = '';
+    }, 1500);
+}
--- homeworks/data-manipulation/data-manipulation.js
+++ homeworks/data-manipulation/data-manipulation.js
@@ -1,7 +1,40 @@
-// Faça uma chamada HTTP para o seguinte endpoint:
-//  https://akabab.github.io/superhero-api/api/all.json
+run();
 
-// Filtre apenas os heróis com a primeira letra do seu nome.
-// Com o resultado da operação anterior, mapeie apenas os heróis que pertencem a DC.
-// Percorra todos os heróis da lista adicionando uma nota aleatória para cada um deles.
-// Conte quantos heróis da Marvel e da DC existem na lista.
+async function run() {
+    const heroes = await getHeroes();
+    console.log(heroes);
+
+    // Encontra o herói favorito.
+    const favoriteHero = heroes.find(hero => hero.name === 'Batman');
+    console.log(favoriteHero);
+
+    // Filtra apenas os heróis com a primeira letra do seu nome.
+    const heroesStartingWithC = heroes.filter(hero => hero.name.substr(0,1) === 'C');
+    console.log(heroesStartingWithC);
+
+    // Mapeia os heróis apenas pelo nome.
+    const heroesNames = heroesStartingWithC.map(hero => hero.name);
+    console.log(heroesNames);
+
+    // Conta quantos heróis da Marvel e da DC existem na lista.
+    const reduceInitialValue = {dc: 0, marvel: 0};
+    const qtdHeroes = heroes.reduce((acc, hero) => {
+        const publisher = hero.biography.publisher;
+        if(publisher === 'DC Comics') {
+            acc.dc += 1;
+        } else {
+            acc.marvel += 1;
+        }
+    }, reduceInitialValue);
+}
+
+// Obtem o JSON.
+async function getHeroes() {
+    try {
+        const requestResult = await fetch('https://akabab.github.io/superhero-api/api/all.json');
+        const jsonResult = await requestResult.json();
+        return jsonResult;
+    } catch (error) {
+        console.error("Error!");
+    }
+}
